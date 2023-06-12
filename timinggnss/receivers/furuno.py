4feb03c2610271d64d1be6473467412f1309318c
SP6HFE <sp6hfe@gmail.com>
2023-06-13 01:24:40 +0200

    Position mode setter added

--- timinggnss/receivers/furuno.py
+++ timinggnss/receivers/furuno.py
@@ -29,6 +29,46 @@ class Furuno:
     def detection_message(self):
         return self.__assemble_message('PERDSYS,VERSION')
 
+    def position_mode_set_message(self, mode: str = 'SS', sigma_threshold: int = 10, time_threshold: int = 1440, latitude: float = 0, longitude: float = 0, altitude: float = 0):
+        # modes: 'NAV'- navigation, 'SS' - self survey, 'CSS' - continous self survey, 'TO' - time only
+        # sigma threshold in meters
+        sigma_threshold_low_limit = 0
+        sigma_threshold_high_limit = 255
+        # time threshold in minutes
+        time_threshold_low_limit = 0
+        time_threshold_high_limit = 10080
+        # latitude and longitude up to seventh decimal place
+        latitude_low_limit = -90
+        latitude_high_limit = 90
+        longitude_low_limit = -180
+        longitude_high_limit = 180
+        # altitude up to two decimal places
+        altitude_low_limit = -1000
+        altitude_high_limit = 18000
+
+        clamped_sigma_threshold = self.__clamp_value(
+            sigma_threshold_low_limit, sigma_threshold, sigma_threshold_high_limit)
+        clamped_time_threshold = self.__clamp_value(
+            time_threshold_low_limit, time_threshold, time_threshold_high_limit)
+        clamped_latitude = self.__clamp_value(
+            latitude_low_limit, self.__limit_float_decimal_places(latitude, 7), latitude_high_limit)
+        clamped_longitude = self.__clamp_value(
+            longitude_low_limit, self.__limit_float_decimal_places(longitude, 7), longitude_high_limit)
+        clamped_altitude = self.__clamp_value(
+            altitude_low_limit, self.__limit_float_decimal_places(altitude, 2), altitude_high_limit)
+
+        query = ''
+        if mode == 'SS':
+            query = 'PERDAPI,SURVEY,1,' + \
+                str(clamped_sigma_threshold) + \
+                ',' + str(clamped_time_threshold)
+        elif mode == 'TO':
+            query = 'PERDAPI,SURVEY,3,0,0,' + \
+                str(clamped_latitude) + ',' + \
+                str(clamped_longitude) + ',' + str(clamped_altitude)
+
+        return self.__assemble_message(query)
+
     def ext_signal_enable_message(self, frequency_hz=10**6, duty=50, offset_to_pps=0):
         frequency_low_limit_hz = 10
         frequency_high_limit_hz = 40*10**6
@@ -68,3 +108,10 @@ class Furuno:
 
     def __clamp_value(self, min, val, max):
         return sorted((min, val, max))[1]
+
+    def __limit_float_decimal_places(self, value: float, max_decimal_places: int):
+        decimal_places = str(value)[::1].find('.')
+        if decimal_places > max_decimal_places:
+            value = int(value * 10**max_decimal_places) / \
+                (10**max_decimal_places)
+        return value
